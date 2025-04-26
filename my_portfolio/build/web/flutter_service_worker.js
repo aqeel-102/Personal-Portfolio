4144266a699794e2c06d2df7d55b4c458a504614
aqeel-102 <aqeelahmad.dev@gmail.com>
2025-04-26 06:14:43 -0700

    Updated resume link to new URL

--- my_portfolio/build/web/flutter_service_worker.js
+++ my_portfolio/build/web/flutter_service_worker.js
@@ -72,7 +72,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "054824e73f1f5c79b5d1b507b39c3870
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "42f1830f9dfd8572502120f04c68d54c",
+"flutter_bootstrap.js": "8a4e523b31b25ce2f19c60e02dffe3e9",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
